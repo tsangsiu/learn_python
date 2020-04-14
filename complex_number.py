ea05cb7a0a2632710596f429330db3e19a6f6783
JT <62657905+tsangsiiu@users.noreply.github.com>
2020-04-14 23:47:24 +0800

    add comments

--- complex_number.py
+++ complex_number.py
@@ -1,3 +1,6 @@
+# complex numbers
+
+# basics syntax
 x = 2 + 3j
 print(type(x))
 
@@ -7,18 +10,20 @@ print(x.conjugate())
 
 #####
 
+# complex function
 a = complex(1,3)
 b = complex(2,4)
 print(a)
 print(b)
 
+# addition
 c = a + b
 print(c)
 
 #####
 
+# more about complex function
 d = complex(15)
-print(d)
-
 e = complex(0,4)
+print(d)
 print(e)
